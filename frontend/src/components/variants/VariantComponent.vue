3dac803ff3c3e68aec196f07ef5c8f6c76926c18
Hannes Drescher <hannescher9@gmail.com>
2023-11-24 15:07:22 +0100

    feat: color the activities accordingly

--- frontend/src/components/variants/VariantComponent.vue
+++ frontend/src/components/variants/VariantComponent.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="variant">
-    <div class="activity" v-for="activity in variant.activities" :key="activity">
+    <div :class="'activity-' + activity.toLowerCase()" v-for="activity in variant.activities" :key="activity">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 36" fill="currentColor" height="4rem">
         <path d="M46 0 0 0 10 18 0 36 46 36 56 18 46 0z"/>
         <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-size=".5em" fill="white">{{ activity }}</text>
@@ -19,7 +19,26 @@ defineProps({
 </template>
 
 <style scoped>
-.activity {
-  display: inline-block;
+.variant {
+  display: flex;
+  flex-direction: row;
+}
+.activity-referral {
+  color: var(--activity-referal-color);
+}
+.activity-evaluation {
+  color: var(--activity-evaluation-color);
+}
+.activity-approach {
+  color: var(--activity-approach-color);
+}
+.activity-authorization {
+  color: var(--activity-authorization-color);
+}
+.activity-procurement {
+  color : var(--activity-procurement-color);
+}
+.activity-transplant {
+  color: var(--activity-transplant-color);
 }
 </style>
