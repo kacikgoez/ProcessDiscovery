ba4d25a677f30917f13ce359592a47d40434b90c
Hannes Drescher <hannescher9@gmail.com>
2023-11-24 16:10:49 +0100

    feat: small improvements and initial chart configuration

--- frontend/src/components/charts/VariantList.vue
+++ frontend/src/components/charts/VariantList.vue
@@ -2,6 +2,18 @@
 import {onMounted, ref} from "vue";
 import {Variant} from "@/types";
 import VariantComponent from "@/components/variants/VariantComponent.vue";
+import { use } from 'echarts/core';
+import { PieChart } from 'echarts/charts';
+import { TooltipComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+import type { ComposeOption } from 'echarts/core';
+import type { PieSeriesOption } from 'echarts/charts';
+import type { TooltipComponentOption } from 'echarts/components';
+
+use([TooltipComponent, PieChart, CanvasRenderer]);
+
+type EChartsOption = ComposeOption<TooltipComponentOption | PieSeriesOption>;
+
 
 const variants = ref<Variant[]>([]);
 
@@ -24,11 +36,46 @@ function fetchVariants() {
             variants.value = data;
         });
 }
+function optionsForVariant(variant: Variant): EChartsOption {
+    return {
+        tooltip: {
+            trigger: 'item',
+        },
+        series: [{
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                    show: true,
+                    fontSize: '20',
+                    fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: Object.entries(variant.distribution).map((key, value) => {
+                return {
+                    name: key,
+                    value: value
+                }
+            })
+        }]
+    }
+
+}
 </script>
 
 <template>
     <div class="variant-list">
-        <VariantComponent v-for="variant in variants" :key="variant.activities" :variant="variant"></VariantComponent>
+      <div class="row" v-for="variant in variants" :key="variant.activities" >
+        <VariantComponent :variant="variant"></VariantComponent>
+      </div>
     </div>
 </template>
 
@@ -36,10 +83,12 @@ function fetchVariants() {
 .variant-list {
   position: relative;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   overflow: scroll;
+}
+.row {
+  margin: 1em;
 }
 </style>
--- frontend/src/components/variants/VariantComponent.vue
+++ frontend/src/components/variants/VariantComponent.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <div class="variant">
     <div :class="'activity-' + activity.toLowerCase()" v-for="activity in variant.activities" :key="activity">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 36" fill="currentColor" height="4rem">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 36" fill="currentColor" height="4rem">
         <path d="M46 0 0 0 10 18 0 36 46 36 56 18 46 0z"/>
         <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-size=".5em" fill="white">{{ activity }}</text>
       </svg>
