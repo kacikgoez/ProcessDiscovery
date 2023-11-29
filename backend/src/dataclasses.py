6fd4773f5e18216f600586e40e50e489df8da059
Hannes Drescher <hannescher9@gmail.com>
2023-11-29 12:21:46 +0100

    feat: basic attribute types and add list of patient attributes that should be extracted

---
+++ backend/src/dataclasses.py
@@ -0,0 +1,50 @@
+import enum
+from dataclasses import dataclass
+
+
+@dataclass
+class CategoricalAttribute:
+    name: str
+    values: list[str]
+
+    def __post_init__(self):
+        self.values = sorted(self.values)
+
+
+@dataclass
+class Interval:
+    lower: float
+    upper: float
+
+    def __post_init__(self):
+        assert self.lower < self.upper, 'The lower bound must be smaller than the upper bound.'
+
+
+@dataclass
+class NumericalAttribute:
+    name: str
+    min: float
+    max: float
+    groups: list[Interval] = None
+
+    def __post_init__(self):
+        assert self.min <= self.max, 'The minimum must be smaller than the maximum.'
+
+    def create_groups(self, n: int):
+        """Create n equally sized groups for the numerical attribute.
+
+        Args:
+            n (int): The number of groups.
+        """
+        self.groups = []
+        step = (self.max - self.min) / n
+        for i in range(n):
+            lower = self.min + i * step
+            upper = lower + step
+            self.groups.append(Interval(lower, upper))
+
+
+@enum.unique
+class AttributeType(enum.Enum):
+    CATEGORICAL = 'categorical'
+    NUMERICAL = 'numerical'
