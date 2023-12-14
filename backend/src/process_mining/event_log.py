f5a14da6963e05ddb7c4b9a7a36e8ffd4ebc238f
Hannes Drescher <hannescher9@gmail.com>
2023-12-15 00:55:46 +0100

    feat: add a filtering function that applies given filters to the event log

--- backend/src/process_mining/event_log.py
+++ backend/src/process_mining/event_log.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-from backend.src.dataclasses import CategoricalAttribute, AttributeType, NumericalAttribute
+from backend.src.dataclasses.attributes import AttributeType, CategoricalAttribute, NumericalAttribute, \
+    DisaggregationAttribute
+from backend.src.dataclasses.filters import FilterOperator, BaseFilter
 from definitions import PATIENT_ATTRIBUTES
 
 
@@ -45,3 +47,43 @@ def load_patient_attributes(event_log: pd.DataFrame) -> list[CategoricalAttribut
             attributes.append(NumericalAttribute(name, event_log[name].min(), event_log[name].max()))
 
     return attributes
+
+
+def create_bins(el: pd.DataFrame, disaggregation_attribute: DisaggregationAttribute | None = None,
+                temporary_column_name='temp') -> pd.DataFrame:
+    # copy the column of the disaggregation attribute to a temporary column to avoid modifying the original data frame
+    if disaggregation_attribute is not None:
+        el[temporary_column_name] = el[disaggregation_attribute.name]
+
+        if disaggregation_attribute.type == AttributeType.NUMERICAL:
+            # bin the numerical values
+            el[temporary_column_name] = pd.cut(el[temporary_column_name],
+                                               bins=disaggregation_attribute.get_bins(),
+                                               labels=disaggregation_attribute.get_bin_labels())
+
+    return el
+
+
+def filter_log(el: pd.DataFrame, filters: list[BaseFilter]) -> pd.DataFrame:
+    for filter in filters:
+        match filter.operator:
+            case FilterOperator.EQUALS:
+                el = el[el[filter.attribute_name] == filter.filter_value]
+            case FilterOperator.NOT_EQUALS:
+                el = el[el[filter.attribute_name] != filter.filter_value]
+            case FilterOperator.CONTAINS:
+                el = el[el[filter.attribute_name].str.contains(filter.filter_value)]
+            case FilterOperator.NOT_CONTAINS:
+                el = el[~el[filter.attribute_name].str.contains(filter.filter_value)]
+            case FilterOperator.LESS_THAN:
+                el = el[el[filter.attribute_name] < filter.filter_value]
+            case FilterOperator.LESS_THAN_OR_EQUALS:
+                el = el[el[filter.attribute_name] <= filter.filter_value]
+            case FilterOperator.GREATER_THAN:
+                el = el[el[filter.attribute_name] > filter.filter_value]
+            case FilterOperator.GREATER_THAN_OR_EQUALS:
+                el = el[el[filter.attribute_name] >= filter.filter_value]
+            case _:
+                raise ValueError('The operator is not supported.')
+
+    return el
