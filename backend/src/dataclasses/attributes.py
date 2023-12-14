b6e71da2de43a3f86f230b14ecd39e0e33af8409
Hannes Drescher <hannescher9@gmail.com>
2023-12-15 00:51:58 +0100

    change: split dataclasses into multiple files

--- backend/src/dataclasses/attributes.py
+++ backend/src/dataclasses/attributes.py
@@ -63,76 +63,9 @@ class DisaggregationAttribute:
         """
         if self.type == AttributeType.NUMERICAL:
             if include_infinities:
-                return [f'< {self.bins[0]}'] + [f'{self.bins[i]} - {self.bins[i + 1]}' for i in range(len(self.bins) - 1)] + [f'> {self.bins[-1]}']
-            else:
-                return [f'{self.bins[i]} - {self.bins[i + 1]}' for i in range(len(self.bins) - 1)]
-        else:
-            raise ValueError('The bin labels are only available for numerical attributes.')
-
-
-@dataclass
-class Variant:
-    activities: list[str]
-    count: int
-    frequency: float
-    distribution: dict[str, int]
-    id: int = field(init=False)
-
-    def __post_init__(self):
-        self.id = hash(tuple(self.activities))
-
-    def get_bin_labels(self, include_infinities: bool = False) -> list[str]:
-        """Returns the bin labels of the numerical attribute.
-
-        Args:
-            include_infinities (bool, optional): Whether to include the infinities in the bin labels. Defaults to False.
-
-        Returns:
-            list[str]: The bin labels.
-        """
-        if self.type == AttributeType.NUMERICAL:
-            if include_infinities:
                 return [f'< {self.bins[0]}'] + [f'{self.bins[i]} - {self.bins[i + 1]}' for i in
                                                 range(len(self.bins) - 1)] + [f'> {self.bins[-1]}']
             else:
                 return [f'{self.bins[i]} - {self.bins[i + 1]}' for i in range(len(self.bins) - 1)]
         else:
             raise ValueError('The bin labels are only available for numerical attributes.')
-
-
-@dataclass
-class Variant:
-    activities: list[str]
-    count: int
-    frequency: float
-    distribution: dict[str, int]
-    id: int = field(init=False)
-
-    def __post_init__(self):
-        self.id = hash(tuple(self.activities))
-
-
-@enum.unique
-class KpiType(enum.Enum):
-    HAPPY_PATH_ADHERENCE = 'happy_path_adherence'
-    DROP_OUT = 'drop_out'
-    PERMUTED_PATH_ADHERENCE = 'permuted_path_adherence'
-    PERMUTED_PATH_DFG = 'permuted_path_dfg'
-    BUREAUCRATIC_DURATION = 'bureaucratic_duration'
-    EVALUATION_TO_APPROACH = 'evaluation_to_approach'
-    AUTHORIZATION_TO_PROCUREMENT = 'authorization_to_procurement'
-
-
-@dataclass
-class KpiRequest:
-    kpi: KpiType
-    disaggregation_attribute: DisaggregationAttribute | None = None
-    legend_attribute: DisaggregationAttribute | None = None
-
-
-@dataclass
-class Filter:
-    filter_attribute: DisaggregationAttribute
-    filter_values: list[str]
-
-
