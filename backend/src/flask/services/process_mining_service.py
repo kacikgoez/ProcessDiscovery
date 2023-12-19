b30605f454f76bfcdcf34e136d4481109aef150a
Hannes Drescher <hannescher9@gmail.com>
2023-12-20 00:45:29 +0100

    fix: use new graph dataclass for DFGs and add necessary functions

--- backend/src/flask/services/process_mining_service.py
+++ backend/src/flask/services/process_mining_service.py
@@ -2,12 +2,12 @@ from typing import Any
 
 import pandas as pd
 
-from backend.src.dataclasses.attributes import CategoricalAttribute, NumericalAttribute, DisaggregationAttribute
-from backend.src.dataclasses.charts import DistributionDataItem
+from backend.src.dataclasses.attributes import CategoricalAttribute, NumericalAttribute
+from backend.src.dataclasses.charts import DistributionDataItem, Graph
 from backend.src.dataclasses.dataclasses import Variant
-from backend.src.dataclasses.requests import KpiRequest, KpiType, VariantListRequest, DistributionRequest
+from backend.src.dataclasses.requests import KpiRequest, KpiType, VariantListRequest, DistributionRequest, DfgRequest
 from backend.src.process_mining.event_log import load_event_log, load_patient_attributes, create_bins, filter_log
-from backend.src.process_mining import kpi
+from backend.src.process_mining import kpi, dfg
 from backend.src.process_mining import distribution
 from backend.src.process_mining.variants import get_variants_with_frequencies
 from definitions import CLEAN_EVENT_LOG_PATH
@@ -54,8 +54,6 @@ class ProcessMiningService:
                 return kpi.get_dropout(el, disaggregation_attribute_column)
             case KpiType.PERMUTED_PATH_ADHERENCE:
                 return kpi.get_permuted_path(el, disaggregation_attribute_column, legend_column)
-            case KpiType.PERMUTED_PATH_DFG:
-                return kpi.get_permuted_path_dfg(el)
             case KpiType.BUREAUCRATIC_DURATION:
                 return kpi.get_bureaucratic_duration(el, disaggregation_attribute_column, legend_column)
             case KpiType.EVALUATION_TO_APPROACH:
@@ -64,3 +62,8 @@ class ProcessMiningService:
                 return kpi.get_authorization_to_procurement(el, disaggregation_attribute_column, legend_column)
             case _:
                 raise ValueError('The given KPI is not supported.')
+
+    def get_dfg(self, request: DfgRequest) -> Graph:
+        el = filter_log(self.event_log, request.filters)
+
+        return dfg.get_permuted_path_dfg(el)
--- backend/src/process_mining/dfg.py
+++ backend/src/process_mining/dfg.py
@@ -1,57 +1,30 @@
-from typing import Dict, List
 import pandas as pd
 from pm4py import discover_dfg
 
-DE_JURE_VARIANT = ('Referral', 'Evaluation', 'Approach', 'Authorization', 'Procurement', 'Transplant')
+from backend.src.dataclasses.charts import Graph, Edge, Node
 
 
-def get_permuted_path_dfg(el: pd.DataFrame, disaggregation_column: str) -> Dict[str, List[Dict[str, str | int]]]:
+def get_permuted_path_dfg(el: pd.DataFrame) -> Graph:
     """
-    Generate a Process Mining Directly-Follows Graph (DFG) based on the given event log.
+    Generate a Process Mining DFG based on the given event log.
 
     Args:
-        el (pd.DataFrame): The event log DataFrame containing process data.
-        disaggregation_column (str): The column name based on which the event log is disaggregated.
-            This column represents the distinct groups for DFG analysis.
+        el (pd.DataFrame): The event log.
 
     Returns:
-        Dict[str, List[Dict[str, Union[str, int]]]]: A dictionary containing:
-            - 'nodes': A list of dictionaries representing nodes with activities and coordinates.
-            - 'links': A list of dictionaries representing directly-following relationships.
+        (Graph): The DFG of the event log with absolute frequencies as edge values.
     """
 
     # find the directly-following graph
-    dfg = el.groupby(disaggregation_column).apply(lambda x: discover_dfg(x, case_id_key='case:concept:name',
-                                                        activity_key='concept:name', timestamp_key='time:timestamp')[0])
+    dfg, start_activities, end_activities = discover_dfg(el)
 
-    nodes = []
-    links = []
-    coordinates = {}
-    rank = 0
-    for index, row in dfg.items():
-        rank = rank + 1
-        for pair, freq in row.items():
-            # find directly-following relations
-            links.append(
-                {
-                    'source': pair[0] + "_" + str(index),
-                    'target': pair[1] + "_" + str(index),
-                    'value': freq
-                }
-            )
+    # transform into graph data structure
+    edges = [Edge(source=source, target=target, label=None, value=freq) for (source, target), freq in dfg.items()]
+    node_ids = set([edge.source for edge in edges] + [edge.target for edge in edges])
+    nodes = [Node(id=activity, label=activity, value=None) for activity in node_ids]
 
-        # Assign coordinates based on activity values
-        for key, value in enumerate(DE_JURE_VARIANT):
-            coordinates[value + "_" + str(index)] = {'x': rank * 5, 'y': key + 20}
-
-            nodes.append(
-                {
-                    'name': value + "_" + str(index),
-                    'x': coordinates[value+ "_" + str(index)]['x'],
-                    'y': coordinates[value+ "_" + str(index)]['y']
-                }
-            )
-
-    return {"nodes": nodes, "links": links}
-
-
+    return Graph(
+        name='DFG',
+        edges=edges,
+        nodes=nodes,
+    )
